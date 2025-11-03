254c3ea6a6a3ccb249b3dae38f2adbf487d88fbe
SebastianVargasOrellana <sebastian.vargas@campus.xaloc.org>
2025-11-03 18:34:10 +0100

    Add files via upload

---
+++ practica_github/Suma amb decimals(act4).py
@@ -0,0 +1,5 @@
+#Pedimos dos valores decimales al usuario y los sumamos
+var1=float(input("Inroduce el valor del numero 1"))
+var2=float(input("Introduce el valor del otro numero"))
+total=var1+var2
+print("La suma de", [var1], "+", [var2],"=",total)
---
+++ practica_github/orden de paraules_act5.py
@@ -0,0 +1,7 @@
+#Pedimos cinco palabras al usuario y las mostramos en orden inverso
+paraula=(input("Introdueix la paraula 1"))
+paraula2=(input("Introdueix la paraula 2"))
+paraula3=(input("Introdueix la paraula 3"))
+paraula4=(input("Introdueix la paraula 4"))
+paraula5=(input("Introdueix la paraula 5"))
+print(paraula,",",paraula2,",",paraula3,",",paraula4,",",paraula5)
