c80bc9153e1fbdd34a38525a40004bb6d1790c3c
SebastianVargasOrellana <sebastian.vargas@campus.xaloc.org>
2026-01-25 23:04:06 +0100

    Add files via upload

---
+++ Actividades listas/act2_lista_palabras.py
@@ -0,0 +1,10 @@
+x = int(input("introduce la cantidad de palabras: "))
+l1=[]
+for i in range(x):
+    p=input(f"Introduce {i+1}ª palabra: ")
+    l1.append(p)
+l1.sort()
+l2=lista1.copy()
+l2.reverse()
+print("lista1 contiene: ", l1)
+print("lista2: contiene", l2)
